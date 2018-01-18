c1183061611bd847ec6d2a5bf174e603a56d040a
Kazuki Ota <kaota@microsoft.com>
2018-01-18 15:01:34 +0900

    simple reactiveproperty demo app impl

---
+++ AppCenterFestival/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Windows.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCenterFestival.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ReactivePropertySlim<string> Input { get; }
+
+        public ReadOnlyReactivePropertySlim<string> Output { get; }
+
+        public MainPageViewModel()
+        {
+            Input = new ReactivePropertySlim<string>("");
+            Output = Input
+                .Delay(TimeSpan.FromSeconds(1))
+                .Select(x => x.ToUpper())
+                .ObserveOnUIDispatcher()
+                .ToReadOnlyReactivePropertySlim();
+        }
+    }
+}
--- AppCenterFestival/Views/MainPage.xaml.cs
+++ AppCenterFestival/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppCenterFestival.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,15 +14,17 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
-namespace AppCenterFestival
+namespace AppCenterFestival.Views
 {
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MainPageViewModel ViewModel => (MainPageViewModel)DataContext;
+
         public MainPage()
         {
             this.InitializeComponent();
